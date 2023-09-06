2b0edd0abec7704fdc57c098cf42d00e04b929ea
shailesh5050 <shaileshkuswaha1@gmail.com>
2023-09-06 09:21:56 +0530

    Added : Logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,8 @@
+import spotify from '../assets/spotify.png'
 const Header = () => {
   return (
     <div className='header'>
-        <h1>Spotify</h1>
+        <img style={{width:'110px'}} src={spotify} alt=""  />
         <img className='header-profile' src="https://freepngimg.com/thumb/facebook/62681-flat-icons-face-computer-design-avatar-icon.png" alt="" />
     </div>
   )
